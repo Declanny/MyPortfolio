e6538d08dd78e1c81015bc0becde4e1301aa07ad
Chisom Henry <chisomhenryg@gmail.com>
2025-11-26 17:31:12 +0100

    refactor: remove NProgress loading indicators from client layout

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import { useEffect } from 'react';
-import { usePathname } from 'next/navigation';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
 import WhatsAppWidget from '../components/WhatsAppWidget';
 
 export default function ClientLayout({
@@ -11,20 +7,6 @@ export default function ClientLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
-  useEffect(() => {
-    const handleStart = () => NProgress.start();
-    const handleStop = () => NProgress.done();
-
-    NProgress.configure({ showSpinner: false });
-    handleStart();
-
-    return () => {
-      handleStop();
-    };
-  }, [pathname]);
-
   return (
     <>
       {children}
